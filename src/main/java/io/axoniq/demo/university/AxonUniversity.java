f21e28b011b253b201b923f8e5a4c4ed4a2c6d6f
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-06-02 17:09:18 +0200

    Add main configurer and description of it
    Add main configurer and description of it
    
    #step1

--- src/main/java/io/axoniq/demo/university/AxonUniversity.java
+++ src/main/java/io/axoniq/demo/university/AxonUniversity.java
@@ -1,5 +1,10 @@
 package io.axoniq.demo.university;
 
+import org.axonframework.common.infra.ComponentDescriptor;
+import org.axonframework.common.infra.FilesystemStyleComponentDescriptor;
+import org.axonframework.configuration.AxonConfiguration;
+import org.axonframework.eventsourcing.configuration.EventSourcingConfigurer;
+
 import java.lang.invoke.MethodHandles;
 import java.util.logging.Logger;
 
@@ -8,6 +13,17 @@ public class AxonUniversity {
     private static final Logger logger = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
 
     public static void main(String[] args) {
-        // ...
+        AxonConfiguration axonConfig = mainConfigurer().build();
+        describe(axonConfig);
+    }
+
+    public static EventSourcingConfigurer mainConfigurer() {
+        return EventSourcingConfigurer.create();
+    }
+
+    private static void describe(AxonConfiguration axonConfig) {
+        ComponentDescriptor componentDescriptor = new FilesystemStyleComponentDescriptor();
+        axonConfig.describeTo(componentDescriptor);
+        logger.info(componentDescriptor.toString());
     }
 }
