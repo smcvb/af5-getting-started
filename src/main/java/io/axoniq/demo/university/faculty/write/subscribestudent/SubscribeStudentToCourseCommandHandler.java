85d003ca9b13b57194fc22ad76d7720dcfec7f34
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-06-05 13:53:20 +0200

    Add decider
    Add decider
    
    #step3

--- src/main/java/io/axoniq/demo/university/faculty/write/subscribestudent/SubscribeStudentToCourseCommandHandler.java
+++ src/main/java/io/axoniq/demo/university/faculty/write/subscribestudent/SubscribeStudentToCourseCommandHandler.java
@@ -1,5 +1,17 @@
 package io.axoniq.demo.university.faculty.write.subscribestudent;
 
+import io.axoniq.demo.university.faculty.events.StudentSubscribedToCourseEvent;
+import org.axonframework.commandhandling.annotation.CommandHandler;
+import org.axonframework.eventhandling.gateway.EventAppender;
+import org.axonframework.modelling.annotation.InjectEntity;
+
 class SubscribeStudentToCourseCommandHandler {
 
+    @CommandHandler
+    public void decide(SubscribeStudentToCourseCommand command,
+                       @InjectEntity SubscribeStudentState state,
+                       EventAppender appender) {
+
+        appender.append(new StudentSubscribedToCourseEvent(command.studentId(), command.courseId()));
+    }
 }
