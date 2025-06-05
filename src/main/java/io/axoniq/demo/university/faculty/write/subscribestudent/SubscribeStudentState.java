0623913a4d7f200cb05ca1390add5c2e58905e3d
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-06-05 13:34:45 +0200

    Add empty SubscribeStudentState
    Add empty SubscribeStudentState
    
    #step3

---
+++ src/main/java/io/axoniq/demo/university/faculty/write/subscribestudent/SubscribeStudentState.java
@@ -0,0 +1,8 @@
+package io.axoniq.demo.university.faculty.write.subscribestudent;
+
+import org.axonframework.eventsourcing.annotation.EventSourcedEntity;
+
+@EventSourcedEntity
+public class SubscribeStudentState {
+
+}
